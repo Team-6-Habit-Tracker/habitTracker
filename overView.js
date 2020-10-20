dcaaa873552325ba64e4d6a11ebec36e96789934
Rafe Nasir <rafenasir@gmail.com>
2020-10-20 13:52:55 +0200

    added different variables with the class names

---
+++ overView.js
@@ -0,0 +1,53 @@
+const fas = "fas";
+const training = 'fa-dumbbell';
+const takingTheStairs = 'fa-walking';
+const reading = 'fa-book';
+const study = 'fa-laptop-code';
+const eatHealthy = 'fa-carrot';
+const goToBedEarly = 'fa-bed';
+const wakeUpEarly = 'fa-clock';
+const noSocialMedia = 'fa-instagram';
+const noSmoking = 'fa-smoking-ban';
+const meditation = 'fa-om';
+
+// let setHabits = function() {
+//         for (let i = 0; i < addedHabits.length; i++) {
+//             let div = document.querySelector('#div' + addedHabits[i].id);
+//             let icon = document.querySelector('#habit' + addedHabits[i].id);
+//             let title = document.querySelector('#title' + addedHabits[i].id);
+
+//             div.classList.remove('hidden');
+
+//             title.innerHTML = addedHabits[i].title;
+
+//             if (addedHabits[i].title == '"Training') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(training);
+//             } else if (addedHabits[i].title == 'Taking the Stairs') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(takingStairs);
+//             } else if (addedHabits[i].title == 'Reading') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(reading);
+//             } else if (addedHabits[i].title == 'Studying') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(study);
+//             } else if (addedHabits[i].title == 'Eating Healthy') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(eatHealthy);
+//             } else if (addedHabits[i].title == 'Go to Bed Early') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(goToBedEarly);
+//             } else if (addedHabits[i].title == 'Wake up Early') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(wakeUpEarly);
+//             } else if (addedHabits[i].title == 'No Socal Media') {
+//                 icon.classList.add('fab');
+//                 icon.classList.add(lessSocialMedia);
+//             } else if (addedHabits[i].title == 'No Smoking') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(noSmoking);
+//             } else if (addedHabits[i].title == 'Meditation') {
+//                 icon.classList.add(fas);
+//                 icon.classList.add(meditation);
+//             };
